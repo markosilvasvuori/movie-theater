dbba26fb268fcd8cfe047faaa4237a772390f8c2
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-12-14 21:32:15 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import Frontpage from './pages/Frontpage';
+import Movies from './pages/Movies';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <Fragment>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Frontpage />} />
+                <Route 
+                    path='/new' 
+                    element={
+                        <Movies 
+                            title={'New Movies'}
+                        />
+                    } 
+                />
+                <Route 
+                    path='/popular' 
+                    element={
+                        <Movies 
+                            title={'Popular Movies'}
+                        />
+                    } 
+                />
+                <Route 
+                    path='/upcoming' 
+                    element={
+                        <Movies 
+                            title={'Upcoming Movies'}
+                        />
+                    } 
+                />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+        </Fragment>
+    );
 }
 
 export default App;
