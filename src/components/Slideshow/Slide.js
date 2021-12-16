23187e376f42153287d984acd7da40b0a2e4df22
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-12-16 18:38:18 +0200

    Added path to 'view more' link

--- src/components/Slideshow/Slide.js
+++ src/components/Slideshow/Slide.js
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 import Button from '../UI/Button';
 import classes from './Slide.module.css';
 
-const Slide = (props) => {
+const Slide = ({ title, rating, releaseDate, backdropImage, movieId}) => {
     return (
-        <div className={classes.slide} style={{ backgroundImage: `url(${props.backdropImage})` }}>
+        <div className={classes.slide} style={{ backgroundImage: `url(${backdropImage})` }}>
             <div className={classes.details}>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <p className={classes.rating}>
-                    <span>{props.releaseDate} | </span>
-                    <span>&#9733; {props.rating}</span>
+                    <span>{releaseDate} | </span>
+                    <span>&#9733; {rating}</span>
                 </p>
                 <div className={classes.links}>
                     <Button className={classes.button}>Get Tickets</Button>
-                    <Link to='/movie'>More info</Link>
+                    <Link to={`/movie/${movieId}`}>More info</Link>
                 </div>
             </div>
         </div>
