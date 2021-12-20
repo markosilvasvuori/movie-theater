1ee2f65fbc8d2c745df33f7bd98fa3c833c0b4cc
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-12-20 22:07:48 +0200

    Removed 'Popular' slider

--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -3,19 +3,14 @@ import Slideshow from "../components/Slideshow/Slideshow";
 import Slider from "../components/Slider/Slider";
 import UpcomingMovies from '../components/Movies/UpcomingMovies';
 
-const Frontpage = ({ nowPlayingMovies, popularMovies, upcomingMovies }) => {
+const Frontpage = ({ nowPlayingMovies, upcomingMovies }) => {
     return (
         <Fragment>
             <Slideshow movies={nowPlayingMovies} />
             <Slider 
                 title={'Now Playing'} 
                 movies={nowPlayingMovies} 
-                categoryPage={'now-playing'} 
-            />
-            <Slider 
-                title={'Popular'} 
-                movies={popularMovies} 
-                categoryPage={'popular'} 
+                categoryPage={'now-playing/0'} 
             />
             <UpcomingMovies movies={upcomingMovies} />
         </Fragment>
