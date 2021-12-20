69d40b0833f8c2623ab91b46743b456c33bd79f3
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-12-20 22:07:00 +0200

    Added styles as variable

--- src/components/UI/HamburgerIcon.js
+++ src/components/UI/HamburgerIcon.js
@@ -1,8 +1,14 @@
 import classes from './HamburgerIcon.module.css';
 
 const MobileNavIcon = (props) => {
+    let styles = `${classes['hamburger-icon']}`;
+
+    if (props.onOpen) {
+        styles = `${classes['hamburger-icon']} ${classes.open}`; 
+    }
+    
     return (
-        <button className={classes['hamburger-icon']} onClick={props.onClick}>
+        <button className={styles} onClick={props.onClick}>
             <div></div>
             <div></div>
             <div></div>
