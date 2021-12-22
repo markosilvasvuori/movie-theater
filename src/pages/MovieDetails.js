ef122d9b922775b88cb45a5e1500481d49107bfe
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-12-22 12:48:02 +0200

    Splitted code

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,9 +2,9 @@ import { Fragment, useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
 import { getMovieDetails } from '../lib/api';
-import Button from '../components/UI/Button';
+import MovieDetailsHero from '../components/Movies/MovieDetails/MovieDetailsHero';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
-import classes from './MovieDetails.module.css';
+import MovieDetailsOverview from '../components/Movies/MovieDetails/MovieDetailsOverview';
 
 const MovieDetails = () => {
     const { movieId } = useParams();
@@ -26,34 +26,10 @@ const MovieDetails = () => {
         <Fragment>
             {isLoading && <LoadingSpinner />}
             {!isLoading &&
-                <div className={classes.container}>
-                    <div className={classes.backdrop} style={{ backgroundImage: `url(${movieDetails.backdrop})` }}></div>
-                    <div className={classes.details}>
-                        <div className={classes.info}>
-                            <h2>{movieDetails.title}</h2>
-                            <p>
-                                <span>{movieDetails.releaseDate} | </span>
-                                <span>&#9733; {movieDetails.rating}</span>
-                            </p>
-                            <Button className={classes.button}>Get Tickets</Button>
-                        </div>
-                        <img className={classes.poster} src={movieDetails.poster} alt={movieDetails.title}/>
-                    </div>
-                </div>
+                <MovieDetailsHero movieDetails={movieDetails} />
             }
             {!isLoading && 
-                <section className={classes['overview-section']}>
-                    <div className={classes.overview}>
-                        <h3>Overview</h3>
-                        <p>{movieDetails.overview}</p>
-                        <Button className={classes.button}>Get Tickets</Button>
-                    </div>
-                    <div className={classes['overview-details']}>
-                        <h3>Details</h3>
-                        <p>Release Date: {movieDetails.releaseDate}<span className={classes.separator}>|</span></p>
-                        <p>&#9733; {movieDetails.rating}</p>
-                    </div>
-                </section>
+                <MovieDetailsOverview movieDetails={movieDetails} />
             }
         </Fragment>
     );
