d5a498f93e74eb55ba92c6d8f1b6767065f9ae91
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-04 11:18:57 +0200

    Added id 'top'

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 
+import { UserContext } from '../../store/user-context';
 import DesktopNavigation from '../Navigation/DesktopNavigation';
 import MobileNavigation from '../Navigation/MobileNavigation';
 import HamburgerIcon from '../UI/HamburgerIcon';
 import Logo from '../UI/Logo';
 import Search from '../Search/Search';
 import Login from '../User/Login';
+import userIcon from '../../img/user.svg';
 import classes from './Header.module.css';
 
 const Header = ({ onShowModal }) => {
+    const {isLoggedIn} = useContext(UserContext);
     const [showMobileNav, setShowMobileNav] = useState(false);
 
     const toggleMobileMenu = () => {
@@ -20,14 +24,21 @@ const Header = ({ onShowModal }) => {
     };
 
     return (
-        <header className={classes.header}>
+        <header id='top' className={classes.header}>
             <Logo onClick={closeMobileMenu} />
             <div className={classes['desktop-nav']}>
                 <DesktopNavigation />
             </div>
             <div className={classes.container}>
                 <Search />
-                <Login onClick={onShowModal} />
+                {!isLoggedIn &&
+                    <Login onClick={onShowModal} />
+                }
+                {isLoggedIn &&
+                    <Link className={classes['user-icon']} to='/userpage'>
+                        <img src={userIcon} alt='user' />
+                    </Link>
+                }
                 <div className={classes['mobile-nav']}>
                     <HamburgerIcon onClick={toggleMobileMenu} onOpen={showMobileNav} />
                     {showMobileNav && 
