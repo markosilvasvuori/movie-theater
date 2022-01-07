183dd5c7f46dbc81b5c9b0fa7262190d6c7d8c59
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-07 14:01:50 +0200

    Moved api key to separate file

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ import NotFoundPoster from '../components/Movies/not-found.png';
 
 export const NOW_PLAYING = `primary_release_date.gte=${getDateThreeMonthsAgo()}&primary_release_date.lte=${getDate()}`;
 export const UPCOMING_MOVIES = `primary_release_date.gte=${getDate(true)}`;
-const API_KEY = '93530160840d922e585f6b81bf62a7a0';
+const API_KEY = process.env.REACT_APP_API_KEY;
 const URL_MOVIES = 'https://api.themoviedb.org/3/discover/movie?api_key=';
 const URL_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500';
