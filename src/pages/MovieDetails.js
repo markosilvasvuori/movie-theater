fa8ab3cd68ae1c216ef17ba8e18d521ed3b603e9
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-11 15:02:46 +0200

    Added isLoggedIn checks

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,18 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, useContext } from 'react';
 import { useParams } from "react-router-dom";
 
 import { getMovieDetails } from '../lib/api';
+import { UserContext } from '../store/user-context'; 
 import MovieDetailsHero from '../components/Movies/MovieDetails/MovieDetailsHero';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
 import MovieDetailsOverview from '../components/Movies/MovieDetails/MovieDetailsOverview';
 import Shows from '../components/Booking/Shows';
+import LoginForm from '../components/User/LoginForm';
+import classes from './MovieDetails.module.css';
 
 const MovieDetails = () => {
     const { movieId } = useParams();
+    const { isLoggedIn } = useContext(UserContext);
     const [isLoading, setIsLoading] = useState(true);
     const [movieDetails, setMovieDetails] = useState(null);
 
@@ -32,7 +36,13 @@ const MovieDetails = () => {
             {!isLoading && 
                 <MovieDetailsOverview movieDetails={movieDetails} />
             }
-            {!isLoading && 
+            {!isLoading && !isLoggedIn &&
+                <div id='shows' className={classes['login-container']}>
+                    <h3>Login to book tickets.</h3>
+                    <LoginForm />
+                </div>
+            }
+            {!isLoading && isLoggedIn &&
                 <Shows 
                     showsAvailable={movieDetails.nowPlaying} 
                     movieTitle={movieDetails.title} 
