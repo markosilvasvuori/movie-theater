fd3df6f44c2d62efb011c15a21ea89ad5d0cb234
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-11 14:24:27 +0200

    Added .container

--- src/components/Movies/MovieDetails/MovieDetailsOverview.js
+++ src/components/Movies/MovieDetails/MovieDetailsOverview.js
@@ -6,23 +6,25 @@ import classes from './MovieDetailsOverview.module.css';
 const MovieDetailsOverview = ({ movieDetails }) => {
     return (
         <section className={classes['overview-section']}>
-            <div className={classes.overview}>
-                <h3>Overview</h3>
-                <p>{movieDetails.overview}</p>
-                <HashLink to='#shows'>
-                    <Button 
-                        className={classes.button}
-                        disabled={movieDetails.nowPlaying ? false : true}
-                    >
-                        Get Tickets
-                    </Button>
-                </HashLink>
-            </div>
-            <div className={classes['overview-details']}>
-                <h3>Details</h3>
-                <p>Release Date: {movieDetails.releaseDate}</p>
-                <span className={classes.separator}>|</span>
-                <p>&#9733; {movieDetails.rating}</p>
+            <div className={classes.container}>
+                <div className={classes.overview}>
+                    <h3>Overview</h3>
+                    <p>{movieDetails.overview}</p>
+                    <HashLink to='#shows'>
+                        <Button 
+                            className={classes.button}
+                            disabled={movieDetails.nowPlaying ? false : true}
+                        >
+                            Get Tickets
+                        </Button>
+                    </HashLink>
+                </div>
+                <div className={classes['overview-details']}>
+                    <h3>Details</h3>
+                    <p>Release Date: {movieDetails.releaseDate}</p>
+                    <span className={classes.separator}>|</span>
+                    <p>&#9733; {movieDetails.rating}</p>
+                </div>
             </div>
         </section>
     );
