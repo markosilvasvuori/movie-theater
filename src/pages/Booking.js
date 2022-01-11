d7b8191966fa3a38db84b38c9d839fa718440c82
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-11 15:01:30 +0200

    Replaced Fragment with .container and changed <Auditorium/> to <Hall/>

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,17 +1,38 @@
-import { Fragment, useContext } from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { ShowsContext } from '../store/shows-context';
-import Auditorium from '../components/Booking/Auditorium';
+import { UserContext } from '../store/user-context';
+import Hall from '../components/Booking/Hall';
+import Button from '../components/UI/Button';
+import classes from './Booking.module.css';
 
 const Booking = () => {
-    const { userBookingData } = useContext(ShowsContext);
+    const { userBookingData } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    if (!userBookingData.title) {
+        navigate(-1);
+    }
 
     return (
-        <Fragment>
-            <h2>{userBookingData.title}</h2>
-            <p>Auditorium {userBookingData.auditorium}</p>
-            <Auditorium />
-        </Fragment>
+        <div className={classes.container}>
+            <header className={classes.header}>
+                <h2>{userBookingData.title}</h2>
+                <p>{userBookingData.date}</p>
+                <p>Hall {userBookingData.hall}</p>
+            </header>
+            {userBookingData.hall && 
+                <Hall 
+                    hallSize={userBookingData.hallSize} 
+                    showDay={userBookingData.showDay}
+                    showDayId={userBookingData.showDayId}
+                    tickets={userBookingData.tickets}
+                /> 
+            }
+            <Button className={classes.button}>
+                Confirm Seats
+            </Button>
+        </div>
     );
 };
 
