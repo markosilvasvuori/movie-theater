7d60c5793b457b77e2e7445eabadc3210cf19d35
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-11 14:19:50 +0200

    Added date selection handling

--- src/components/Booking/Shows.js
+++ src/components/Booking/Shows.js
@@ -1,15 +1,17 @@
-import { Fragment, useContext, useState } from 'react';
+import { Fragment, useContext, useState, useRef, useEffect } from 'react';
 
 import { ShowsContext } from '../../store/shows-context';
 import ShowItem from './ShowItem';
 import classes from './Shows.module.css';
 
 const Shows = ({ showsAvailable, movieTitle }) => {
-    const { DUMMY_DATA } = useContext(ShowsContext);
+    const { DEMO_DATA } = useContext(ShowsContext);
+    const dateRef = useRef();
     const [showDay, setShowday] = useState('showDay1');
+    const [showDate, setShowDate] = useState('');
     const [tickets, setTickets] = useState(1);
-
     let dateCounter = 0;
+
     const getDate = () => {
         const date = new Date();
         date.setDate(date.getDate() + dateCounter);
@@ -20,16 +22,25 @@ const Shows = ({ showsAvailable, movieTitle }) => {
    
         dateCounter++;
 
-        return `${year}.${month}.${day}`;
+        return `${day}.${month}.${year}`;
     };
 
     const onChangeSelectHandler = (event) => {
-        setShowday(event.target.value);
+        const target = event.target;
+        setShowday(target.value);
+        setShowDate(target.options[target.selectedIndex].text);
     };
 
     const onChangeTicketsHandler = (event) => {
         setTickets(event.target.value);
     };
+
+    useEffect(() => {
+        if (showsAvailable) {
+            const options = dateRef.current.options;
+            setShowDate(options[options.selectedIndex].text);
+        }
+    }, [dateRef, showsAvailable]);
 
     return (
         <Fragment>
@@ -39,7 +50,12 @@ const Shows = ({ showsAvailable, movieTitle }) => {
                     <form className={classes.form}>
                         <div className={classes['form-column']}>
                             <h4>Date</h4>
-                            <select id='dates' name='dates' onChange={onChangeSelectHandler}>
+                            <select 
+                                id='dates' 
+                                name='dates' 
+                                ref={dateRef} 
+                                onChange={onChangeSelectHandler}
+                            >
                                 <option value='showDay1'>Today {getDate()}</option>
                                 <option value='showDay2'>Tomorrow {getDate()}</option>
                                 <option value='showDay3'>{getDate()}</option>
@@ -50,7 +66,11 @@ const Shows = ({ showsAvailable, movieTitle }) => {
                         </div>
                         <div className={classes['form-column']}>
                             <h4>Tickets</h4>
-                            <select id='tickets' name='tickets' onChange={onChangeTicketsHandler}>
+                            <select 
+                                id='tickets' 
+                                name='tickets' 
+                                onChange={onChangeTicketsHandler}
+                            >
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
@@ -60,14 +80,17 @@ const Shows = ({ showsAvailable, movieTitle }) => {
                         </div>
                     </form>
                     <ul>
-                        {DUMMY_DATA[showDay].map(show => (
+                        {DEMO_DATA[showDay].map(show => (
                             <ShowItem 
                                 key={show.id}
                                 id={show.id}
+                                showDay={showDay}
+                                date={showDate}
                                 title={movieTitle}
                                 showTime={show.time}
-                                auditoriumNumber={show.auditorium}
-                                freeSeats={show.freeSeats}
+                                hallNumber={show.hall}
+                                hallSize={show.hallSize}
+                                seats={show.seats}
                                 tickets={tickets}
                             />
                         ))}
@@ -75,7 +98,9 @@ const Shows = ({ showsAvailable, movieTitle }) => {
                 </div>
             }
             {!showsAvailable && 
-                <p className={classes['no-shows']}>No shows available for this title.</p>
+                <p className={classes['no-shows']}>
+                    No shows available for this title.
+                </p>
             }
         </Fragment>
     );
