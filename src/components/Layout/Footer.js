60c5ff362aaf1d71e668b47d07a7252ca03d6f95
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-20 14:00:31 +0200

    Formatting changes

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -22,7 +22,9 @@ const Footer = () => {
                         >
                             <img 
                                 className={classes['tmdb-logo']}
-                                src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg' alt='TMDB' />
+                                src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg' 
+                                alt='TMDB'
+                            />
                         </a>
                         <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
                     </div>
