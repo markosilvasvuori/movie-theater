9528a8919a89b9c879d30f6809c609b45b63c675
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-20 13:50:38 +0200

    Added .ticket-container

--- src/components/User/UserDashboard/BookedMovie.js
+++ src/components/User/UserDashboard/BookedMovie.js
@@ -1,18 +1,46 @@
+import { useState } from "react";
+
 import Button from "../../UI/Button";
+import Modal from "../../UI/Modal";
+import Ticket from "./Ticket";
 import classes from './BookedMovie.module.css';
 
-const BookedMovie = ({ title, date, time, amount }) => {
+const BookedMovie = ({ title, date, time, tickets, row, seats }) => {
+    const [showTicketInModal, setShowTicketInModal] = useState(false);
+
+    const showTicket = () => {
+        setShowTicketInModal(!showTicketInModal);
+    };
+
     return (
+        <>
+        {showTicketInModal &&
+            <Modal title='Tickets' onClick={showTicket}>
+                <div className={classes['ticket-container']}>
+                    {seats.map(ticket => (
+                        <Ticket
+                            key={ticket}
+                            title={title}
+                            date={date}
+                            time={time}
+                            row={row}
+                            seat={ticket}
+                        />
+                    ))}
+                </div>
+            </Modal>
+        }
         <li className={classes['booked-movie']}>
             <h4>{title}</h4>
             <div className={classes.details}>
                 <p><small>{date} | {time}</small></p>
                 <div className={classes.column}>
-                    <p><small>Tickets: {amount}</small></p>
-                    <Button>Show ticket</Button>
+                    <p><small>Tickets: {tickets}</small></p>
+                    <Button onClick={showTicket}>Show ticket</Button>
                 </div>
             </div>
         </li>
+        </>
     );
 };
 
