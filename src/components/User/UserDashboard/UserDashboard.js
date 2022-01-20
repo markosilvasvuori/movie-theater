84297d9f4f24445dd8276fdd78978032a6277a85
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-01-20 13:49:28 +0200

    Added generateKey() function

--- src/components/User/UserDashboard/UserDashboard.js
+++ src/components/User/UserDashboard/UserDashboard.js
@@ -1,30 +1,41 @@
 import { useContext } from 'react';
-import { UserContext } from '../../../store/user-context';
 
+import { AuthContext } from '../../../store/auth-context';
+import { UserContext } from '../../../store/user-context';
 import BookedMovie from './BookedMovie';
 import Button from '../../UI/Button';
 import classes from './UserDashboard.module.css';
 
 const UserDashboard = () => {
-    const {userData, logOutHandler} = useContext(UserContext);
+    const { authCtxValues } = useContext(AuthContext);
+    const { userData } = useContext(UserContext);
+
+    const generateKey = () => {
+        return Math.random().toString(36).slice(2);
+    };
 
     return (
         <div className={classes['user-dashboard']}>
             <div className={classes.container}>
                 <header className={classes.header}>
                     <h2>Hello {userData.username}!</h2>
-                    <Button onClick={logOutHandler}>Log out</Button>
+                    <Button onClick={authCtxValues.logout}>Log out</Button>
                 </header>
                 <div>
                     <h3>Booked Tickets</h3>
+                    {userData.bookedTickets.length === 0 &&
+                        <p>No booked tickets.</p>
+                    }
                     <ul className={classes['booked-movies']}>
                         {userData.bookedTickets.map(ticket => (
                             <BookedMovie 
-                                key={ticket.title}
+                                key={generateKey()}
                                 title={ticket.title}
                                 date={ticket.date}
                                 time={ticket.time}
-                                amount={ticket.amount}
+                                tickets={ticket.tickets}
+                                row={ticket.row}
+                                seats={ticket.seats}
                             />
                         ))}
                     </ul>
